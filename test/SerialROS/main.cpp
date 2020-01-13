eba9cb2908b289fbf31477597d1918e8b2ed9e71
emzarem <emurphyzaremba@gmail.com>
2020-01-13 16:31:52 -0500

    Updating ROS test to confirm data content

--- test/SerialROS/main.cpp
+++ test/SerialROS/main.cpp
@@ -1,41 +1,50 @@
+#include "SerialPacket.h"
+
 #include <Arduino.h>
+#include <vector>
 
 #define STD_LED 13
 
-void serial_setup()
-{
+SerialUtils::CmdMsg expected = {0, 1, 1, 1};
+
+void serial_setup() {
     Serial.begin(115200);
 
-    while(!Serial){};
+    while (!Serial) {};
     Serial.println("Hello");
 }
 
-void blink()
-{
+void blink() {
     digitalWrite(STD_LED, 1);
-    delay(250);
+    delay(100);
     digitalWrite(STD_LED, 0);
-    delay(250);
+    delay(100);
     digitalWrite(STD_LED, 1);
-//    delay(250);
-//    digitalWrite(STD_LED, 0);
-//    delay(250);
-//    digitalWrite(STD_LED, 1);
+    //    delay(250);
+    //    digitalWrite(STD_LED, 0);
+    //    delay(250);
+    //    digitalWrite(STD_LED, 1);
 }
 
-void setup() 
-{
+void setup() {
     serial_setup();
-  
+
     pinMode(STD_LED, OUTPUT);
     digitalWrite(STD_LED, 1);
 
+    std::vector<char> bytes;
 
-    while(1) {
-        while(Serial.available() > 0) {
-            Serial.read();
+    while (1) {
+        bytes.clear();
+        while (Serial.available() > 0) { bytes.push_back(Serial.read()); }
+        SerialUtils::CmdMsg rcvd = {0};
+        SerialUtils::unpack(bytes, rcvd);
+
+        if (rcvd.is_relative == expected.is_relative &&
+            rcvd.m1_angle == expected.m1_angle &&
+            rcvd.m2_angle == expected.m2_angle &&
+            rcvd.m3_angle == expected.m3_angle)
             blink();
-        }
     };
 }
 
