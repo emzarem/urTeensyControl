532349ae35a719bcfaa91bd4432258865c79852d
emzarem <emurphyzaremba@gmail.com>
2020-01-20 10:19:34 -0500

    Adding sleep and reset pins

--- src/util.h
+++ src/util.h
@@ -8,7 +8,6 @@ const uint8_t PIN_LIM1 = 15;
 const uint8_t PIN_LIM2 = 16;
 const uint8_t PIN_LIM3 = 17;
 
-
 // LEDs
 const uint8_t STD_LED = 13;
 
@@ -18,6 +17,8 @@ const int PIN_MISO = 12;
 const int PIN_SCK = 14;
 
 // Motors
+const int PIN_N_SLP = 6;
+const int PIN_RST = 7;
 const int PIN_M1_CS = 8;
 const int PIN_M2_CS = 9;
 const int PIN_M3_CS = 10;
